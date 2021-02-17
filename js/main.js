ae1aa2da119682462f76f413666c1ee444f597b9
baimyrza09 <baimyrzabeishenaliev@gmail.com>
2021-02-17 11:50:10 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,62 @@
+
+// let number = 123;
+// let output = [];
+// let sNumber = number.toString();
+
+// func(sNumber);
+
+// function func(sNumber) {
+//     let sum = 0;
+//     for (let i = 0; i < sNumber.length; i++) {
+//         output.push(+sNumber[i]);
+//     }
+//     for (let i = 0; i < sNumber.length; i++) {
+//         sum += output[i];
+//     }
+//     console.log(sum);
+// }
+
+
+// let arr = [1,2,3,4,];
+// let newArr = arr.join('');
+// console.log(newArr);
+
+
+// let str = "Askat,baimyrza,Alisher";
+
+// // let regex = new RegExp(/^[A-Z, А-Я]/, 'g')
+// let regex = /^[A-Z, А-Я]/g
+
+// // console.log(regex.test(str))
+
+// console.log()
+
+
+//  let str = prompt('enter date of birth');
+// const regexp = new RegExp(/\d{2}.\d{2}.\d{4}/);
+// let result = regexp.test(str);
+// console.log(result)
+
+// let arr = [
+//    [ 11, 2, 4,],
+//    [ 4, 5, 6,],
+//    [ 10, 8, -12]
+// ]
+// let firstDiagonal = 0
+// let secondDiagonal = 0
+// let sum = 0
+// for(let i=0; i<arr.length; i++){
+//     firstDiagonal+=arr[i][i]
+// }
+
+// for(let j=arr.length-1; j>0; j--){
+//     console.log(secondDiagonal)
+//     // secondDiagonal =secondDiagonal+  arr[j][j]
+//     secondDiagonal = secondDiagonal+  arr[j-1   ][j-1]
+// }
+// // console.log(firstDiagonal)
+// // console.log(secondDiagonal)
+//  sum = Math.abs(firstDiagonal-secondDiagonal)
+    // console.log(sum)
+
+    
